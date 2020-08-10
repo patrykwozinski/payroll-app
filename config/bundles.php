3658dad1719191f85af0ddea6b0f0798b9fe355c
Patryk Woziński <patryk.wozinski@gmail.com>
2020-08-10 11:12:00 +0200

    nelmio + functional test for command + some more

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ tests/Payroll/Functional/UserInterface/Cli/CreateDepartmentTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Payroll\Functional\UserInterface\Cli;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Tester\CommandTester;
+
+final class CreateDepartmentTest extends KernelTestCase
+{
+    private Command $createDepartment;
+    private CommandTester $commandTester;
+
+    protected function setUp(): void
+    {
+        $kernel = static::bootKernel();
+        $application = new Application($kernel);
+
+        $this->createDepartment = $application->find('payroll:create-department');
+        $this->commandTester = new CommandTester($this->createDepartment);
+    }
+
+    public function testDepartmentCreatedInfo(): void
+    {
+        $this->commandTester->execute([
+            'command' => $this->createDepartment->getName(),
+            'name' => 'IT operations',
+            'bonus-type' => 'Yearly',
+            'bonus-value' => 700,
+        ]);
+        $output = $this->commandTester->getDisplay();
+
+        self::assertStringContainsString('Created department!', $output);
+    }
+}
